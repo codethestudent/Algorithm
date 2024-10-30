51f1a1ec1e678e49c8b09fd58299a434877b031f
하준영 <hjun303@gnu.ac.kr>
2024-10-30 21:45:26 +0900

    Title: 특정 위치의 숫자, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 241030/특정 위치의 숫자/numbers-in-specific-location.java
+++ 241030/특정 위치의 숫자/numbers-in-specific-location.java
@@ -6,7 +6,11 @@ public class Main {
         int answer = 0;
         Scanner sc = new Scanner(System.in);
         for(int i = 0; i<10; i++){
-            answer += sc.nextInt();
+            if(i == 2 || i== 4 || i == 9){
+                answer += sc.nextInt();
+            } else {
+                sc.nextInt();
+            }
         }
         System.out.println(answer);
     }
