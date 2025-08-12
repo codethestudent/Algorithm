b971f9644674a22b44a5b9d5d421dec4690aea8f
하준영 <hjun303@gnu.ac.kr>
2025-08-12 10:52:38 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,82 +1,139 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-class Node {
-    char value;
-    Node left;
-    Node right;
-
-    public Node(char value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static Node[] tree;
-
-    // 전위 순회
-    public static void preorder(Node node) {
-        if (node == null) return;
-        System.out.print(node.value);
-        preorder(node.left);
-        preorder(node.right);
-    }
-
-    // 중위 순회
-    public static void inorder(Node node) {
-        if (node == null) return;
-        inorder(node.left);
-        System.out.print(node.value);
-        inorder(node.right);
-    }
-
-    // 후위 순회
-    public static void postorder(Node node) {
-        if (node == null) return;
-        postorder(node.left);
-        postorder(node.right);
-        System.out.print(node.value);
-    }
+    static Map<String, Node> nodes = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        tree = new Node[N + 1]; // 노드 배열 생성
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine()); // 띄어쓰기 기준으로 문자열 분리
-            char parentValue = st.nextToken().charAt(0); // nextToken() 메서드로 토큰을 하나씩 꺼내오면 StringTokenizer객체에는 해당 토큰이 사라진다
-            char leftValue = st.nextToken().charAt(0);
-            char rightValue = st.nextToken().charAt(0);
-            
-            // Java에서 char 데이터 타입은 내부적으로 ASCII 코드를 사용
-            if (tree[parentValue - 'A'] == null) { // 부모 노드가 아직 생성되지 않은 경우. 'A'는 문자 'A'의 ASCII 값
-                tree[parentValue - 'A'] = new Node(parentValue); // 부모 노드를 생성
-            }
-            if (leftValue != '.') { // 왼쪽 자식이 존재할 경우
-                tree[leftValue - 'A'] = new Node(leftValue); // 왼쪽 자식 노드를 생성
-                tree[parentValue - 'A'].left = tree[leftValue - 'A']; // 부모 노드와 연결
-            }
-            if (rightValue != '.') { // 오른쪽 자식이 존재할 경우
-                tree[rightValue - 'A'] = new Node(rightValue); // 오른쪽 자식 노드를 생성
-                tree[parentValue - 'A'].right = tree[rightValue - 'A']; // 부모 노드와 연결
+            for (int i = 0; i < N; i++) {
+                String[] arr = br.readLine().split(" ");
+                String root = arr[0];
+                String left = arr[1];
+                String right = arr[2];
+
+                Node rootNode = null;
+                if (!".".equals(root) && nodes.get(root) == null) {
+                    rootNode = new Node(root);
+                    nodes.put(root, rootNode);
+                } else {
+                    rootNode = nodes.get(root);
+                }
+
+                Node leftNode = null;
+                if (!".".equals(left) && nodes.get(left) == null) {
+                    leftNode = new Node(left);
+                    nodes.put(left, leftNode);
+                } else {
+                    leftNode = nodes.get(left);
+                }
+
+                Node rightNode = null;
+                if (!".".equals(right) && nodes.get(right) == null) {
+                    rightNode = new Node(right);
+                    nodes.put(right, rightNode);
+                } else {
+                    rightNode = nodes.get(right);
+                }
+
+                if (rootNode != null && leftNode != null) {
+                    leftNode.parent = rootNode;
+                    rootNode.left = leftNode;
+                }
+
+                if (rootNode != null && rightNode != null) {
+                    rightNode.parent = rootNode;
+                    rootNode.right = rightNode;
+                }
+
             }
         }
 
-        // 전위 순회
-        preorder(tree[0]);
-        System.out.println();
+        // for (Map.Entry<String, Node> entry : nodes.entrySet()) {
+        //     System.out.println(entry.getKey() + " -> " + entry.getValue());
+        // }
 
-        // 중위 순회
-        inorder(tree[0]);
-        System.out.println();
+        preOrder(nodes.get("A"));
+        System.out.println(preSb);
+        inOrder(nodes.get("A"));
+        System.out.println(inSb);
+        postOrder(nodes.get("A"));
+        System.out.println(postSb);
+    }
 
-        // 후위 순회
-        postorder(tree[0]);
-        System.out.println();
+    private static StringBuilder preSb = new StringBuilder();
+    private static StringBuilder inSb = new StringBuilder();
+    private static StringBuilder postSb = new StringBuilder();
+
+    // root -> left -> right
+    private static void preOrder(Node node) {
+        preSb.append(node.id);
+        if (node.left != null) {
+            preOrder(node.left);
+        }
+        if (node.right != null) {
+            preOrder(node.right);
+        }
+    }
+
+    // left -> root -> right
+    private static void inOrder(Node node) {
+        if (node.left != null) {
+            inOrder(node.left);
+        }
+        inSb.append(node.id);
+        if (node.right != null) {
+            inOrder(node.right);
+        }
+    }
+
+    // left -> right -> root
+    private static void postOrder(Node node) {
+        if (node.left != null) {
+            postOrder(node.left);
+        }
+        if (node.right != null) {
+            postOrder(node.right);
+        }
+
+        postSb.append(node.id);
+    }
+
+    public static class Node {
+        String id;
+        Node parent;
+        Node left;
+        Node right;
+
+        public Node(String id) {
+            this.id = id;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Node other = (Node) obj;
+            if (id == null) {
+                if (other.id != null)
+                    return false;
+            } else if (!id.equals(other.id))
+                return false;
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "id: " + id
+                    + " parent : " + (parent == null ? null : parent.id)
+                    + " left : " + (left == null ? null : left.id)
+                    + ", right : " + (right == null ? null : right.id);
+        }
     }
 }
