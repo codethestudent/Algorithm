594d3cdeb9c8e627f6e2322fa597d87e78353f5f
하준영 <hjun303@gnu.ac.kr>
2025-08-21 17:54:42 +0900

    [Silver II] Title: 랜선 자르기, Time: 192 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int K = arr[0];
+            int N = arr[1];
+
+            long[] lanCables = new long[K];
+            long hi = 0;
+
+            for (int i = 0; i < K; i++) {
+                lanCables[i] = Long.parseLong(br.readLine());
+                hi = hi < lanCables[i] ? lanCables[i] : hi;
+            }
+
+            long lo = 1;
+            long count = 0;
+            while (lo <= hi) {
+                count = 0;
+                long mid = (lo + hi) / 2;
+
+                for (int i = 0; i < K; i++) {
+                    count += lanCables[i] / mid;
+                    if (count >= N) {
+                        break;
+                    }
+                }
+
+                if (count >= N) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+
+            System.out.println(hi);
+
+        }
+    }
+}
