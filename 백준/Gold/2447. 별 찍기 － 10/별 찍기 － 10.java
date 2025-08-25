16a47d850ea804c89333fa51b8a77306c4d1f109
하준영 <hjun303@gnu.ac.kr>
2025-08-25 18:21:45 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 376 ms, Memory: 35484 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,43 +1,49 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	
-	static int N;
-	static String[][] arr;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		arr = new String[N][N];
-		for(int i=0;i<N;i++)
-			Arrays.fill(arr[i]," ");
-		star(0,0,N);
-		
-		 for (int i = 0; i < N; i++) {
-	            for (int j = 0; j < N; j++) {
-	            	sb.append(arr[i][j]);
-	            }
-	            sb.append("\n");
-		 }
-		System.out.println(sb);
-	}// main()
-	
-	static void star(int x, int y, int n) {
-		if(n==1) {
-			arr[x][y]="*";
-			return;
-		}
-		
-		 for (int i = 0; i < 3; i++) {
-	            for (int j = 0; j < 3; j++) {
-	                if (!(i == 1 && j == 1)) {
-	                    star( x + i * (n / 3), y + j * (n / 3), n / 3);
-	                }
-	            }
-	        }
-		
-	}//star()
+    private static String[][] answer;
 
-}// class Main
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int n = Integer.parseInt(br.readLine());
+            answer = new String[n][n];
+
+            stars(n, 0, 0);
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (answer[i][j] == null) {
+                        bw.write(" ");
+                        continue;
+                    }
+                    bw.write(answer[i][j]);
+                }
+                bw.write("\n");
+            }
+            bw.flush();
+        }
+
+    }
+
+    private static void stars(int n, int row, int col) {
+
+        if (n == 1) {
+            answer[row][col] = "*";
+            return;
+        }
+
+        int depth = n / 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                stars(depth, row + i * depth, col + j * depth);
+            }
+        }
+    }
+
+}
